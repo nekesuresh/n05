44635ffbb4d5553d956eaa6eda1b66f0c1665a47
nekesuresh <88456215+nekesuresh@users.noreply.github.com>
2024-12-03 17:05:26 -0500

    Made changes

--- n05-backend/server.js
+++ n05-backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public/n05-frontend/browser', 'index.csr.html'));
 });
 
 
